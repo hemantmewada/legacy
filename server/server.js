247c8dbcb93028d9561eec7f44c422698faaa4be
Hemant <hmewada74@gmail.com>
2024-06-05 18:08:03 +0530

    home route added

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ const app = express()
 // application level middlewares
 app.use(cors())
 app.use(express.json())
-
+app.use("/", async (req, res) => {
+  return res.status(200).send({
+    status: true,
+    message: "This domain's API managed by @hemantmewada"
+  })
+})
 app.use("/api",enquiryRouter);
 
 mongoConnect().then(() => {
